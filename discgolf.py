59a2f5c4aa61dcfa107e280d7f0cae93de572450
mistersam11 <samroth011@gmail.com>
2026-05-04 22:52:00 -0400

    New discgolf file

--- discgolf.py
+++ discgolf.py
@@ -1,81 +1,16 @@
-import re
-import string
 import random
+import os
+import string
+import re
 
-# Create a parsing class that takes user input and compares it with a set of actions and discs -----------------
-class Parser:
-    def __init__(self, player_input):
-        # Initialize the objects/variables
-        self.throw_types = ['backhand','forehand','putt']
-        self.disc_types = ['driver','midrange','putter']
-        self.unwanted_words = ['a','the','an','at','to','with','throw']
-        self.player_input = player_input
-        self.normalized_input = self.normalize(player_input)
-
-    def normalize(self, player_input):
-        # Remove capitalization and punctuation
-        normal_text = player_input.lower()
-        translator = str.maketrans('', '', string.punctuation)
-        normal_text = normal_text.translate(translator)
-        
-        # Convert string into a list of words
-        split_text = normal_text.split()
-
-        # Filter out all words but Type of Throw and Type of Disc
-        filtered_list = [word for word in split_text if word not in self.unwanted_words]
-
-        # If remaining words aren't in [0] throw_types and [1] disc_types, return false.
-        if len(filtered_list) > 1:
-            if filtered_list[0] in self.throw_types and filtered_list[1] in self.disc_types:
-               return filtered_list
-            else:
-                return None
-        else:
-            return None
-
-# Zone Dictionaries ---------------------------------------------------------------------------
-
-class Circle1:
-    def __init__(self, distance, disc_type, throw_type):
-        self.desc = f"The {disc_type} glides through the air, landing gracefully inside circle one, {distance} away from the basket!"
-        self.type = "fairway"
-        self.range = 30
-
-class Circle2:
-    def __init__(self, distance, disc_type):
-        self.desc = f"The {disc_type} glides through the air, landing gracefully inside circle two, {distance} feet away from the basket!"
-        self.type = "fairway"
-        self.range = 60
-
-class Fairway:
-    def __init__(self, distance, disc_type, throw_type):
-        self.desc = f"With a snap of the wrist, the disc follows a gentle S-curve and finds center fairway, {distance} feet away from the basket!"
-        self.type = "fairway"
-        self.range = 1000
-
-class RoughShallow:
-    def __init__(self, distance, disc_type, throw_type):
-        self.desc = f"What a bad shot selection! The {disc_type} kareens into a tree and flutters harmlessly down into a bush. Good look throwing from {distance} feet to the basket."
-        self.type = "rough_shallow"
-        self.range = 1000
-
-class RoughDeep:
-    def __init__(self, distance, disc_type, throw_type):
-        self.desc = f"DISASTER! WEEEOOOOEEEEEOOOOOEEEEEOOOOOOOOO YOUR DISC IS GONE FOREVER GOSH DANG IT OH MAN GOSH DARNBETTER LUCK NEXT TIME YOU BOZO. {distance} FEET FROM THE BASKET!!!!!!!!"
-        self.type = "rough_deep"
-        self.range = 1000
-
-# Exit Command List ---------------------------------------------------------------------
-exit_phrases = ['q','quit','exit','stop']
-
-#Disc classes ---------------------------------------------------------------------------
+# Disc library with accuracy and distance values for each disc type and throw type
 class Driver:
     def __init__(self, throw_type):
         if throw_type == "backhand":
-            self.accuracy = 75
-            self.distance = 400
+            self.accuracy = 60
+            self.distance = 375
         elif throw_type == "forehand":
-            self.accuracy = 90
+            self.accuracy = 80
             self.distance = 325
         else:
             self.accuracy = 100
@@ -85,9 +20,9 @@ class Midrange:
     def __init__(self, throw_type):
         if throw_type == "backhand":
             self.accuracy = 85
-            self.distance = 300
+            self.distance = 275
         elif throw_type == "forehand":
-            self.accuracy = 96
+            self.accuracy = 90
             self.distance = 230
         else:
             self.accuracy = 100
@@ -105,146 +40,120 @@ class Putter:
             self.accuracy = 100
             self.distance = 40
 
-# Hole Dictionary -----------------------------------------------------------------------------------
+# Parser function to extract throw type and disc type from user input
+def parse_text(player_input: str):
+    """Parse the input and return (throw_type, disc_type) or (None, None)."""
+    throw_types = ['backhand', 'forehand', 'putt']
+    disc_types = ['driver', 'midrange', 'putter']
+    unwanted_words = ['a', 'the', 'an', 'at', 'to', 'with', 'throw', 'want', 'i']
 
-class Hole1():
-    def __init__(self):
-        self.distance = 400
+    # Normalize input
+    normal_text = player_input.lower()
+    translator = str.maketrans('', '', string.punctuation)
+    normal_text = normal_text.translate(translator)
+    split_text = normal_text.split()
 
-# Function to handle throws and next-zone placement -------------------------------------------------
-def throw_shot(throw_type, disc_type, current_distance, current_zone):
-    def shot_variance():
-        random_number = random.random()
-        if random_number > 0.5:
-            return -1
-        else:
-            return 1
-    
-    # Declare variables
-    random_number2 = random.random() + 0.1
-    variance = shot_variance()
-    thrown_disc = None   
+    # Filter out filler words
+    filtered_list = [word for word in split_text if word not in unwanted_words]
 
-    # Gather class data based on user input
-    match disc_type: 
-        case "driver":
-            thrown_disc = Driver(throw_type)
-        case "midrange":
-            thrown_disc = Midrange(throw_type)
-        case "putter":
-            thrown_disc = Putter(throw_type)   
-        case _:
-            print("It sure wasn't a beaver in the pond...")
+    if len(filtered_list) > 1 and filtered_list[0] in throw_types and filtered_list[1] in disc_types:
+        return filtered_list[0], filtered_list[1]
 
-    # Calculate throw distance
-    throw_distance = round(thrown_disc.distance + (
-       thrown_disc.distance * 0.2 * random_number2 * variance
-    ))
+    return None, None
 
-    # Apply accuracy penalty based on current zone
-    penalty = 0
-    if current_zone.type == "rough_shallow":
-        penalty = 20
-    elif current_zone.type == "rough_deep":
-        penalty = 40
+# Get the accuracy for a regular shot
+def shot_accuracy_check(throw_type, disc_type, current_zone):
+    if disc_type == "driver":
+        disc = Driver(throw_type)
+    elif disc_type == "midrange":
+        disc = Midrange(throw_type)
     else:
-        penalty = 0
+        disc = Putter(throw_type)
+
+    # Adjust accuracy based on current zone
+    if current_zone == "fairway":
+        accuracy = disc.accuracy
+    elif current_zone == "rough_shallow":
+        accuracy = disc.accuracy - 20
+    elif current_zone == "rough_deep":
+        accuracy = disc.accuracy - 30
+    else:  # hazard
+        accuracy = disc.accuracy - 20
+
+    # Simulate the throw and determine if it's accurate
+    roll = random.randint(1, 100)
+    return roll <= accuracy
 
 
-    # Shot accuracy check (returns True or False)
-    def check_accuracy(accuracy_percent):
-        return random.random() <= (accuracy_percent / (100 + penalty))
-    
-    # Calculate the new zone the player ends up in
-    def get_zone(current_distance):
-        if current_distance > 60:
-            return Fairway(current_distance, disc_type, throw_type)
-        elif current_distance > 30:
-            return Circle2(current_distance, disc_type)
-        else:
-            return Circle1(current_distance, disc_type, throw_type)
-
-    # Run the actual throw 
-    if check_accuracy(thrown_disc.accuracy): 
-        current_distance = current_distance - throw_distance
-        current_zone = get_zone(current_distance) 
-        print("Good throw!")
+# Returns throw_distance and potential_zone (either one of the roughs or None) and is_accurate
+def get_shot_distance(throw_type, disc_type, current_zone):
+    if disc_type == "driver":
+        disc = Driver(throw_type)
+    elif disc_type == "midrange":
+        disc = Midrange(throw_type)
     else:
-        print("Bad throw!")
-        random_num = random.random()
-        if random_num < 0.5:
-            throw_distance = int(throw_distance * 0.5)
-            current_distance = current_distance - throw_distance
-            current_zone = get_zone(current_distance)
+        disc = Putter(throw_type)
+
+    if shot_accuracy_check(throw_type, disc_type, current_zone):
+        return int(disc.distance + ((random.random() + 0.1) * 50)), None, True
+    else:
+        if random.random() > 0.5:
+            return int((disc.distance + ((random.random() + 0.1) * 50)) / 2), None, False
         else:
-            random_num2 = random.random()
-            if random_num2 < 0.5: 
-                current_distance = current_distance - throw_distance
-                current_zone = RoughShallow(current_distance, disc_type, throw_type)
-            else: 
-                current_distance = current_distance - throw_distance
-                current_zone = RoughDeep(current_distance, disc_type, throw_type)
+            if random.random() > 0.5:
+                return int((disc.distance + ((random.random() + 0.1) * 50)) / 4), "rough_shallow", False
+            else:
+                return int((disc.distance + ((random.random() + 0.1) * 50)) / 8), "rough_deep", False
 
-    print(f"You threw it {throw_distance} feet!")
-    print(current_zone.desc)
+# Calculate putt accuracy (used when in circle 1 or 2). Returns True or False
+def putt_accuracy_check(current_zone):
+    if current_zone == "circle2":
+        return random.random() <= 0.4
+    else:
+        return random.random() <= 0.89
+        
+# Calculate putt distance (used when in circle 1 or 2 and putt is inaccurate). Returns the updated current_distance and a past_basket variable to use for messages
+def get_putt_distance(current_distance):
+    updated_distance = current_distance - current_distance + random.randint(1, 20)
+    if updated_distance <= 5:
+        return updated_distance, False
+    else:
+        return updated_distance, True
+    
+# Determine the new current zone based on the shot results ()
+def get_next_zone(current_distance, potential_zone):
+    if potential_zone == None:
+        if current_distance <= 30:
+            return "circle1"
+        elif current_distance <= 60:
+            return "circle2"
+        else:
+            return "fairway"
+    else: return potential_zone
 
-    return current_distance, current_zone
-
-# Define the current hole via class --------------------------------------------------------------
-def init_hole(current_hole):
-    match current_hole:
-        case 1:
-            return Hole1()
-        case 2:
-            return Hole2()
-        case 3:
-            return Hole3()
-        case 4:
-            return Hole4()
-        case 5:
-            return Hole5()
-        case 6:
-            return Hole6()
-        case 7:
-            return Hole7()
-        case 8:
-            return Hole8()
-        case 9:
-            return Hole9()
-
-# Initalize the round --------------------------------------------------------------------------
-
-hole_start = True
-this_hole = None
+# Declare wide-scope variables to be updated through game loop
+throw_type = None
+disc_type = None
 current_hole = 1
-current_distance = None
 current_par = 3
-current_throw = 1
-current_zone = Fairway(None, None, None)
+current_distance = 410
+current_zone = None
+made_putt = False
+is_accurate = None
+odd_choice = False
+hole_start = True
 
-print("Welcome to BIG DISC GOLF GAME! To play, just type 'Throw a backhand with a driver', or 'Forehand a putter', etc. and see what happens!")
-
-# Main loop -----------------------------------------------------------------------------------      
 while True:
-    
-    #Initialize the current hole
+
     if hole_start:
-        this_hole = init_hole(current_hole)
-        current_distance = this_hole.distance
-        print(f"HOLE {current_hole}. Par: {current_par}. Distance: {current_distance}.")
+        print(f"Hole: {current_hole}, Par: {current_par}, Distance: {current_distance} feet.")
         hole_start = False
+        current_zone = "fairway"
 
-    # Process the parsed user command, execute the throw, and print the result
-    user_cmd = input(">")
-    cmd_output = Parser(user_cmd)
-    if user_cmd in exit_phrases:
-        break
-    elif cmd_output.normalized_input is None:
-        print("Make sure to include the throw type and disc name! Try again.")
-    else:
-        throw_type = cmd_output.normalized_input[0]
-        disc_type = cmd_output.normalized_input[1]
-        current_distance, current_zone = throw_shot(throw_type, disc_type, current_distance, current_zone)
+    throw_type, disc_type = parse_text(input("> "))
 
+    #Quitting the program if anything other than a correct command is entered (for now)
+    if disc_type == None: break
 
-
+    if current_zone != "circle1" or "circle2":
+        throw_distance, potential_zone, is_accurate = get_shot_distance(throw_type, disc_type, current_zone)
